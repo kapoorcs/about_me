d2d2dc57b26c3eee131c42921dfa11abfffe07ed
Tarun Gupta <tarun.gupta@venturepact.com>
2013-12-28 09:51:13 +0530

    hello world

--- routes/index.js
+++ routes/index.js
@@ -6,3 +6,7 @@
 exports.index = function(req, res){
   res.render('index', { title: 'Express' });
 };
+
+exports.helloworld = function(req, res){
+    res.render("Hello World", {"title": "Yo Yo Hello !!"})
+};
